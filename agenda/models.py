d2754661359a60c4c2d7acb31d577e0fe904187f
migueleichler <migueleichler23@gmail.com>
2017-11-10 00:10:40 -0200

    Adição do teste de campos obrigatorios no model Compromisso.

--- agenda/models.py
+++ agenda/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Compromisso(models.Model):
     titulo = models.CharField(max_length=100)
+    horario = models.DateTimeField()
+    local = models.CharField(max_length=100)
+    observacao = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.titulo
