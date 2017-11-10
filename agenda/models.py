f0a3b3f869d4565980ce23fb058d81a9c7667efe
migueleichler <migueleichler23@gmail.com>
2017-11-10 14:25:23 -0200

    Correção do teste de campos obrigatorios do model Compromisso.

--- agenda/models.py
+++ agenda/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Compromisso(models.Model):
-    titulo = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100, null=False)
     horario = models.DateTimeField()
     local = models.CharField(max_length=100)
     observacao = models.CharField(max_length=200, blank=True, null=True)
--- agenda/tests/test_models.py
+++ agenda/tests/test_models.py
@@ -3,25 +3,15 @@ from django.test import TestCase
 from agenda.models import Compromisso
 from model_mommy import mommy
 
-from datetime import datetime
-
 
 class AgendaModelsTest(TestCase):
 
     def setUp(self):
         self.instance = mommy.make('Compromisso')
-        self.obligatory_fields = {
-                                  'titulo': 'Jantar',
-                                  'horario': datetime.now(),
-                                  'local': 'Casa',
-                                 }
 
     def test_string_representation(self):
         self.assertEqual(str(self.instance), self.instance.titulo)
 
     def test_obrigatory_fields(self):
-        created = Compromisso.objects.create(
-                      titulo=self.obligatory_fields.get('titulo'),
-                      horario=self.obligatory_fields.get('horario'),
-                      local=self.obligatory_fields.get('local'))
+        created = Compromisso.objects.create(horario=self.instance.horario)
         self.assertTrue(isinstance(created, Compromisso))
