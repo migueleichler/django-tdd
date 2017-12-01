dd01aab5eaf48810c0ad70edc2ceee547efae2c6
migueleichler <migueleichler23@gmail.com>
2017-12-01 01:52:58 -0200

    Teste dos métodos Get e Post na view de criação de novos compromissos.

--- agenda/tests/test_views.py
+++ agenda/tests/test_views.py
@@ -1,7 +1,10 @@
-from django.test import TestCase, RequestFactory, Client
+from django.test import TestCase, RequestFactory
 from django.core.urlresolvers import reverse
 
+
 from model_mommy import mommy
+from agenda.views import CreateCompromisso
+from agenda.models import Compromisso
 
 
 class CompromissoViewTest(TestCase):
@@ -12,10 +15,18 @@ class CompromissoViewTest(TestCase):
                              'horario': self.instance.horario,
                              'local': self.instance.local,
                              'observacao': self.instance.observacao}
-        self.client = Client()
+        self.factory = RequestFactory()
 
-    def test_create_compromisso(self):
-        response = self.client.post(reverse('compromisso_novo'),
-                                    self.request_data)
+    def test_get(self):
+        request = self.factory.get(reverse('compromisso_novo'))
+        response = CreateCompromisso.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        request = self.factory.post(reverse('compromisso_novo'), self.request_data)
+
+        response = CreateCompromisso.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Compromisso.objects.last().titulo, self.instance.titulo)
